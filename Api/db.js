376cb0803ce3bfee2632e2d86b16c7dd9e017004
blackoin-studio <uriellexa99@gmail.com>
2024-12-28 14:42:27 -0500

    Api folder structures

---
+++ Api/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    dbName: "b2win",
+  });
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
+};
+
+module.exports = connectDB;
